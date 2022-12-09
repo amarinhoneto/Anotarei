b9015599b0459e53b4c271d5b372eaff0c1df57c
Ailton Marinho <ailton.mneto@hotmail.com>
2022-12-09 07:20:17 -0300

    service ok

--- anotarei/src/main/java/anotarei/anotarei/service/AnotacaoService.java
+++ anotarei/src/main/java/anotarei/anotarei/service/AnotacaoService.java
@@ -1,14 +1,35 @@
 package anotarei.anotarei.service;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import anotarei.anotarei.model.Anotacao;
 import anotarei.anotarei.repository.AnotacaoRepository;
+import java.util.List;
+import java.util.Optional;
+
 
 public class AnotacaoService {
     
     @Autowired
     private AnotacaoRepository repository;
 
-    public void teste(){
+    public List<Anotacao> buscaAnotacaos(){
+        return repository.findAll();
     }
     
+    public List<Anotacao> buscarAnotacaoPorTitulo(String titulo){
+        return repository.findBytitulo(titulo);
+    }
+
+    public Optional<Anotacao> buscarPorId(Integer id){
+        return repository.findById(id);
+    }
+
+    public Anotacao salvar(Anotacao anotacao){
+        return repository.save(anotacao);
+    }
+
+    public void deletar (Integer id){
+        repository.deleteById(id);
+    }
 }
